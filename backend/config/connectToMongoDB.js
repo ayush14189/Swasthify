e820699bd2106cef34865380b40edb724d797f84
Ayush Jain <ayushjain14189@gmail.com>
2025-04-13 02:56:04 +0530

    Uploaded Docker Files

--- backend/config/connectToMongoDB.js
+++ backend/config/connectToMongoDB.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const process = require("process");
-
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/swasthify";
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(MONGO_URI);
     console.log("MongoDB connected!!");
   } catch (error) {
     console.log("Failed to connect to MongoDB", error);
